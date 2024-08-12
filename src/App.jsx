46dbd6ee555d18821b214ea1ec2007fa3bd5b62c
Adityaaery20 <112109873+Adityaaery20@users.noreply.github.com>
2024-08-12 13:59:40 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect  } from "react"
 import Todolist from "./components/Todolist"
-import Todoinput from "./components/TodoInput"
+import Todoinput from "./components/Todoinput"
 
 function App() {
 
--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 export default function Todoinput(props) {
     const {handleAddTodos, todoValue, setTodoValue} = props
 
